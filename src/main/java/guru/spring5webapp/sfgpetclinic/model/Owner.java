c134cf9296502aa2f1c95ae7f0032dca809e6f7f
PC <anindya.das93@gmail.com>
2019-04-09 14:46:11 +0530

    Pet Clinic POJO

---
+++ src/main/java/guru/spring5webapp/sfgpetclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package guru.spring5webapp.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+
+}
---
+++ src/main/java/guru/spring5webapp/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.spring5webapp.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+
+
+}
